13d7635da429d90754ab5f2a35197d53a646cfbe
Asilbek <asilbek.sadullayev000@gmail.com>
2026-06-05 18:01:12 +0500

    fix: add Pillow and AUTH_USER_MODEL setting

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Custom User Model
+AUTH_USER_MODEL = 'core.CustomUser'
+
 # Django REST Framework Configuration
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
